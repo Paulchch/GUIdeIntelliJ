944cc301b2974e49519469f6d5d4835865b9294b
ESCINF <pablitombcr@gmail.com>
2022-08-30 10:32:16 -0600

    Update

---
+++ src/main/java/personas/View.java
@@ -0,0 +1,15 @@
+package personas;
+
+import javax.swing.*;
+
+public class View {
+    private JPanel panel;
+    private JLabel id_lbl;
+    private JTextField id_fld;
+    private JLabel nombre_lbl;
+    private JTextField nombre_fld;
+    private JLabel Sexo;
+    private JRadioButton masculino_fld;
+    private JRadioButton Femenino_fld;
+    private JLabel estado_lbl;
+}
